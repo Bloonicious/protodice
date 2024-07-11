b312897d14bca05104651198398bdcbd1962d919
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-07-11 22:58:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 var app = angular.module('tdGameApp', ['dndLists']);
+
+app.service('ConfigService', ['$http', function($http) {
+    this.loadDefenses = function() {
+        return $http.get('config/defenses.json').then(response => response.data);
+    };
+    
+    this.loadMonsters = function() {
+        return $http.get('config/monsters.json').then(response => response.data);
+    };
+}]);
