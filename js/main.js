0e40d6fa7155516fb0d114173350c20e2540a3ab
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-07-12 16:10:36 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 var app = angular.module('tdGameApp', ['dndLists']);
 
+app.service('ConfigService', ['$http', function($http) {
+    this.loadDefenses = function() {
+        return $http.get('config/defences.json').then(function(response) {
+            return response.data;
+        });
+    };
+
+    this.loadMonsters = function() {
+        return $http.get('config/monsters.json').then(function(response) {
+            return response.data;
+        });
+    };
+}]);
+
 app.controller('GameController', ['$scope', 'ConfigService', function($scope, ConfigService) {
     // UI flags
     $scope.showPlaySectionFlag = false;
